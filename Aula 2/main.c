10cd41556c4f5a5dd1dfaa9be9cdfdc61918b00c
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-03-07 11:26:54 -0300

    Add files via upload

---
+++ Aula 2/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World! \n");
+    printf("Bom dia \n");
+    return 0; //Para a execução
+    printf("Boa tarde \n");
+    printf("Boa noite \n");
+}
