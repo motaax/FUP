4965f28e5b545630a17748ab26ee1fa1f752c790
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-03-07 12:44:47 -0300

    Add files via upload

---
+++ Exercicios/contando.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("Contando: 1 \n");
+    printf("Contando: 2 \n");
+    printf("Contando: 3 \n");
+    printf("Contando: 4 \n");
+    printf("Contando: 5 \n");
+    printf("Contando: 6 \n");
+    printf("Contando: 7 \n");
+    printf("Contando: 8 \n");
+    printf("Contando: 9 \n");
+    printf("Contando: 10");
+    
+    return 0;
+}
