4fc0f8ec266de5dcd0dac65741a8e67f77a518a6
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-03-12 15:08:24 -0300

    Add files via upload

---
+++ Aula 4/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 7;
+    printf("Valor de x: %d \n", x);
+    x = x * 2; //x=14
+    x = x + 3; //x=17
+    x = x * 2; //x=34
+    printf("Valor de x: %d \n", x);
+
+    return 0;
+}
