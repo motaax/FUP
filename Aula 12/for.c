5c2119c85a474562e4f15ca009932ed7e4f7dac1
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-04-10 14:07:24 -0300

    Add files via upload

---
+++ Aula 12/for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    COM WHILE
+    int i = 0;
+    while(i < 10) {
+        printf("%d \n", i);
+        i++;
+    }
+    */
+
+    for(int i = 0; i < 10; i++) {
+        printf("%d \n", i);
+    }
+
+    return 0;
+}
