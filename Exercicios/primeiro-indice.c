eacf7555168c81460db326dbb856e88a886b9173
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-04-22 13:43:40 -0300

    Add files via upload

---
+++ Exercicios/primeiro-indice.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int vetor[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &vetor[i]);
+    }
+
+    int maior = vetor[0];
+    int indice = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (vetor[i] > maior) {
+            maior = vetor[i];
+            indice = i;
+        }
+    }
+
+    printf("Valor: %d\n", maior);
+    printf("Primeiro indice: %d\n", indice);
+
+    return 0;
+}
