3fbda7badc5fdc0f46b71dfa5b180997fa96393c
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-04-24 14:13:21 -0300

    Add files via upload

---
+++ Exercicios/palindromo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char palavra[51]; 
+    int i, tamanho, palindromo = 1;
+
+    scanf("%s", palavra);
+
+    tamanho = strlen(palavra);
+
+    for(i = 0; i < tamanho / 2; i++) {
+        if(palavra[i] != palavra[tamanho - 1 - i]) {
+            palindromo = 0;
+            break;
+        }
+    }
+
+    if(palindromo)
+        printf("Palindromo\n");
+    else
+        printf("Nao palindromo\n");
+
+    return 0;
+}
