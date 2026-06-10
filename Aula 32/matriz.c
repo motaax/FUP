a0a64fab7d5c87b5f6f3756d98e511caf9cb23f9
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-06-10 14:23:26 -0300

    Add files via upload

--- Aula 32/matriz.c
+++ Aula 32/matriz.c
@@ -7,14 +7,19 @@ int main() {
     for(int l = 0; l < 2; l++) {
         //c = coluna
         for(int c = 0; c < 3; c++) {
+            printf("Digite o valor da linha %d coluna %d\n", l, c);
             scanf("%f", &matriz[l][c]);
         }
     }
 
+    //l = linha
     for(int l = 0; l < 2; l++) {
+        //c = coluna
         for(int c = 0; c < 3; c++) {
-            printf("%.2f \n", matriz[l][c]);
+            printf("%.1f ", matriz[l][c]);
         }
+
+        printf("\n");
     }
 
     return 0;
