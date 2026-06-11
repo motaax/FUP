3030bbbb316748a46b89d1989368772d5a5bf886
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-06-11 12:57:05 -0300

    Add files via upload

---
+++ Exercicios/inverta-nomes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char nome1[100], nome2[100], nome3[100];
+    scanf("%s", nome1);
+    scanf("%s", nome2);
+    scanf("%s", nome3);
+
+    printf("%s\n", nome3);
+    printf("%s\n", nome2);
+    printf("%s\n", nome1);
+
+    return 0;
+}
