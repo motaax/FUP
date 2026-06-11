df71869b5a1d445911d4955af9f94a05335ae8fb
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-06-11 13:56:39 -0300

    Add files via upload

---
+++ Aula 33/matriz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char nomes[3][50];
+    scanf("%s", nomes[0]);
+    scanf("%s", nomes[1]);
+    scanf("%s", nomes[2]);
+
+    printf("%s \n", nomes[0]);
+    printf("%s \n", nomes[1]);
+    printf("%s \n", nomes[2]);
+
+    return 0;
+}
