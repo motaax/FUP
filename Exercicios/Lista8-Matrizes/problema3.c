6b3641f7d7b8fc723f9acbe9f629c95400b123fe
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-06-11 10:30:57 -0300

    Add files via upload

---
+++ Exercicios/Lista8-Matrizes/problema3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[4][4];
+    
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            matriz[i][j] = i * j;
+        }
+    }
+
+    printf("Matriz 4x4: \n");
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
