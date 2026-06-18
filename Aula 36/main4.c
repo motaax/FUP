cbcee00296a27fde74dd593ecd89326c4d6bebdb
Felipe Mota <86206681+motaax@users.noreply.github.com>
2026-06-18 14:03:30 -0300

    Add files via upload

--- Aula 36/main4.c
+++ Aula 36/main4.c
@@ -5,9 +5,12 @@ typedef struct{
 } Ponto;
 
 int main(){
-    Ponto p = {2, 3};
+    Ponto p1 = {2, 3};
+    Ponto p2;
+    p2 = p1; //A linguagem C permite atribuições entre structs
 
-    printf("p = (%.1f, %.1f)\n", p.x, p.y);
+    printf("p = (%.1f, %.1f)\n", p1.x, p1.y);
+    printf("p = (%.1f, %.1f)\n", p2.x, p2.y);
 
     return 0;
 }
